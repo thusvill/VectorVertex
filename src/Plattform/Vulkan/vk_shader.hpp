13745e882b4fa7025073e1cd29db715e66ef6642
thusvill <thusvill@gmail.com>
2024-11-05 10:43:28 +0530

    shaderc

--- src/Plattform/Vulkan/vk_shader.hpp
+++ src/Plattform/Vulkan/vk_shader.hpp
@@ -7,68 +7,27 @@ namespace VectorVertex
     class VKShader : public Shader
     {
     public:
-        VKShader() = default;
         VKShader(const std::string &filepath);
-        VKShader(const std::string &filepath, VkShaderStageFlagBits stage) : shaderStage(stage)
-        {
-            auto shaderCode = readFile(filepath);
-            createShaderModule(shaderCode);
-        }
-        ~VKShader()
-        {
-            if (shaderModule != VK_NULL_HANDLE)
-            {
-                vkDestroyShaderModule(VKDevice::Get().device(), shaderModule, nullptr);
-            }
-        }
+        VKShader(const std::string &name, const std::string &vertexSrc, const std::string &fragmentSrc);
 
-        VkPipelineShaderStageCreateInfo getShaderStageInfo() const
-        {
-            VkPipelineShaderStageCreateInfo shaderStageInfo{};
-            shaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-            shaderStageInfo.stage = shaderStage;
-            shaderStageInfo.module = shaderModule;
+        virtual ~VKShader();
+        virtual void Bind() override;
+        virtual void Unbind() override;
+        virtual void SetInt(const std::string &name, int value) override;
+        virtual void SetIntArray(const std::string &name, int *values, uint32_t count) override;
+        virtual void SetFloat(const std::string &name, float value) override;
+        virtual void SetFloat2(const std::string &name, const glm::vec2 &value) override;
+        virtual void SetFloat3(const std::string &name, const glm::vec3 &value) override;
+        virtual void SetFloat4(const std::string &name, const glm::vec4 &value) override;
+        virtual void SetMat4(const std::string &name, const glm::mat4 &value) override;
+        
+        private:
+            std::unordered_map<VkFlags, std::string> PreProcess(const std::string &source);
 
-            shaderStageInfo.pName = (shaderStage == VK_SHADER_STAGE_VERTEX_BIT) ? "vertex" : "fragment";
+            std::unordered_map<VkShaderStageFlagBits, std::vector<uint32_t>> m_VulkanSPIRV;
+            std::unordered_map<VkShaderStageFlagBits, VkShaderModule> m_ShaderModules;
 
-            return shaderStageInfo;
-        }
-        VkShaderModule getModule() const { return shaderModule;}
-
-    private:
-        VkShaderModule shaderModule = VK_NULL_HANDLE;
-        VkShaderStageFlagBits shaderStage;
-
-        std::vector<char> readFile(const std::string &filepath)
-        {
-            std::ifstream file(filepath, std::ios::ate | std::ios::binary);
-
-            if (!file.is_open())
-            {
-                throw std::runtime_error("failed to open file: " + filepath);
-            }
-
-            size_t fileSize = static_cast<size_t>(file.tellg());
-            std::vector<char> buffer(fileSize);
-
-            file.seekg(0);
-            file.read(buffer.data(), fileSize);
-
-            file.close();
-            return buffer;
-        }
-
-        void createShaderModule(const std::vector<char> &code)
-        {
-            VkShaderModuleCreateInfo createInfo{};
-            createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-            createInfo.codeSize = code.size();
-            createInfo.pCode = reinterpret_cast<const uint32_t *>(code.data());
-
-            if (vkCreateShaderModule(VKDevice::Get().device(), &createInfo, nullptr, &shaderModule) != VK_SUCCESS)
-            {
-                throw std::runtime_error("failed to create shader module!");
-            }
-        }
-    };
-}
+            void CreateShaderModule(VkShaderStageFlagBits stage, const std::vector<uint32_t> &code);
+            void CompileAndCacheShaders(const std::unordered_map<VkShaderStageFlagBits, std::string> &shaderSources);
+            
+    }
