08dca20b7e4a8880ed1728ab67bbab78d6b31f12
Bios <thusvill@gmail.com>
2026-01-03 13:15:32 +0530

    Engines compiles without errors

--- Engine/build/cmake_install.cmake
+++ Engine/build/cmake_install.cmake
@@ -49,6 +49,11 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
+  include("/home/bios/VectorVertex/Engine/build/src/vendor/spdlog/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
   include("/home/bios/VectorVertex/Engine/build/src/vendor/yaml-cpp/cmake_install.cmake")
 endif()
 
